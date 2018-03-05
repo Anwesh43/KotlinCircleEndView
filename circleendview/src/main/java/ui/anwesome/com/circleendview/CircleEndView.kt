b159a7b473a70b91ad930e68599e406da2727435
Anwesh43 <anweshthecool0@gmail.com>
2018-03-06 02:00:55 +0530

    adding Animator class

--- circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
+++ circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
@@ -42,4 +42,29 @@ class CircleEndView(ctx : Context) : View(ctx) {
             }
         }
     }
+    data class Animator(var view : CircleEndView, var animated : Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex : Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
