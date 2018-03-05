1069a7fa4528c2f899893fc1f17c0c8e33ba69e6
Anwesh43 <anweshthecool0@gmail.com>
2018-03-06 02:18:03 +0530

    adding Renderer in View

--- circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
+++ circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
@@ -9,13 +9,14 @@ import android.content.*
 import android.graphics.*
 class CircleEndView(ctx : Context, var n : Int = 5) : View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
