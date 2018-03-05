4d36f6ce15d448b05447080ac9d2814403421c89
Anwesh43 <anweshthecool0@gmail.com>
2018-03-06 01:53:16 +0530

    adding circleendview class

--- circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
+++ circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.circleendview
 /**
  * Created by anweshmishra on 06/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class CircleEndView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
