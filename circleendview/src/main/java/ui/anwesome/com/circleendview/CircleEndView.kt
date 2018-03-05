f5b9e31be029e4d4d94d8879699a07475d0d6447
Anwesh43 <anweshthecool0@gmail.com>
2018-03-06 02:11:40 +0530

    adding CircleExpand class

--- circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
+++ circleendview/src/main/java/ui/anwesome/com/circleendview/CircleEndView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.circleendview
 import android.view.*
 import android.content.*
 import android.graphics.*
-class CircleEndView(ctx : Context) : View(ctx) {
+class CircleEndView(ctx : Context, var n : Int = 5) : View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas : Canvas) {
 
@@ -65,6 +65,34 @@ class CircleEndView(ctx : Context) : View(ctx) {
             if(animated) {
                 animated = false
             }
+        }
+    }
+    data class CircleExpand(var i : Int, var n : Int) {
+        val state = State()
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w = canvas.width.toFloat()
+            val h = canvas.height.toFloat()
+            val r = Math.min(w,h)/20
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.parseColor("#4CAF50")
+            paint.strokeWidth = r/12
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.save()
+            canvas.translate(r + (w -r - r) * state.scales[1], h/2)
+            val y_gap = h / (2 * n +1)
+            for(i in 0..n-1) {
+                canvas.save()
+                canvas.translate(0f, (i - n/2) * y_gap * state.scales[2])
+                canvas.drawArc(RectF(-r, -r, r, r), 0f, 360f * state.scales[0], false, paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
         }
     }
 }
